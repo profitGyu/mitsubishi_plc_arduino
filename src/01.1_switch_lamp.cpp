537bf0a202a19f8f534b912ac47e7321d4d0d686
profitgyu <profitgyu@profitgyuui-MacBookPro.local>
2026-06-16 00:05:27 +0900

    feat: 01강 스위치 램프 주석 추가

--- src/01.1_switch_lamp.cpp
+++ src/01.1_switch_lamp.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 /* 램프와 스위치 다루기
-   1. 1번 핀 스위치를 on 하면 1번 lamp가 on이 되도록 (a 접점 형식으로) on 이 유지가 되지 않으면 꺼지도록
+  1번 핀 스위치를 on 하면 1번 lamp가 on이 되도록 
+  2번 핀 스위치를 on 하면 2번 lamp가 off가 되도록 
 */
 
 // put function declarations here:
